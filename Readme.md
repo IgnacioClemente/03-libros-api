117d860fc50324ed991b928dd8b3f3358fde1f82
Nacho <ignagus4432@gmail.com>
2024-10-28 20:22:32 -0300

    Added readme

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Api - books
+- ``` npm init ```
+- ``` npm install express  ```
+- ``` npm install prisma @prisma/client  ```
+- ``` npx prisma init ```
+
+## Comandos Prisma 
+-  Genera una migracion y aplica en el datasource dicha migracion
+- ``` npx prisma migrate dev ```
+- Ver estado de migraciones aplicadas
+- ``` npx prisma migrate status ```
+
+## Variables de Entorno
+- APIBOOK_PORT =
+- APIBOOK_JWT_TIME = 
+- APIBOOK_JWT_PASSWORD = 
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
 import 'dotenv/config'
 import booksRoutes from './src/books/book.routes.js'
 import authorRoutes from './src/author/author.routes.js'
 import userRoutes from './src/user/user.routes.js'
 import authRoutes from './src/auth/auth.routes.js'
 
+
 const app = express();
 const port = parseInt(process.env.APIBOOK_PORT);
 
+app.use(morgan('combined'));
 app.use(express.json());
 
 app.get('/', (req,res) =>{
